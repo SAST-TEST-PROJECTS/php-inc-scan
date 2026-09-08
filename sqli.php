ec51a181cfa99eb500aedf076fa3a5ce50f4becd
Jan Kühl <jan.kuhl@mend.io>
2026-09-08 06:01:56 +0200

    third commit

--- sqli.php
+++ sqli.php
@@ -5,7 +5,7 @@ $userId = $_GET['id'];
 
 $conn = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), 'app');
 
-$query = "SELECT id, name, email FROM users WHERE id = " . $userId;
+$query = "SELECT id, name, email FROM users WHERE id = 23"
 $result = mysqli_query($conn, $query);
 
 $row = mysqli_fetch_assoc($result);
